955f270656bc11f8c5d062388d72dabfbf99a199
lvshudi <lvshudi119@126.com>
2019-08-27 11:27:28 +0800

    first recuriting

--- src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
+++ src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
@@ -6,35 +6,38 @@ import java.util.List;
 import java.util.Vector;
 
 public class OwingPrinter {
-    void printOwing(String name, List<Order> orders) {
-        Iterator<Order> elements = orders.iterator();
-        double outstanding = 0.0;
+	void printOwing(String name, List<Order> orders) {
+		Iterator<Order> elements = orders.iterator();
+		double outstanding = 0.0;
 
-        // print banner
-        System.out.println ("*****************************");
-        System.out.println ("****** Customer totals ******");
-        System.out.println ("*****************************");
+		// print owings
+		while (elements.hasNext()) {
+			Order each = (Order) elements.next();
+			outstanding += each.getAmount();
+		}
+		
 
-        // print owings
-        while (elements.hasNext()) {
-            Order each = (Order) elements.next();
-            outstanding += each.getAmount();
-        }
+	}
 
-        // print details
-        System.out.println("name: " + name);
-        System.out.println("amount: " + outstanding);
-    }
+	public void printBannerAndDetails(String name, double outstanding) {
+		// print banner
+		System.out.println("*****************************");
+		System.out.println("****** Customer totals ******");
+		System.out.println("*****************************");
+		// print details
+		System.out.println("name: " + name);
+		System.out.println("amount: " + outstanding);
+	}
 }
 
 class Order {
-    private final double amount;
+	private final double amount;
 
-    public Order(double amount) {
-        this.amount = amount;
-    }
+	public Order(double amount) {
+		this.amount = amount;
+	}
 
-    public double getAmount() {
-        return amount;
-    }
+	public double getAmount() {
+		return amount;
+	}
 }
